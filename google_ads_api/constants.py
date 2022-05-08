ab6e422098f6b76762872d52ca13cf3ae570f413
brad <bradisbon@gmail.com>
2022-05-08 12:37:22 -0500

    constants.py: move values to env vars

--- google_ads_api/constants.py
+++ google_ads_api/constants.py
@@ -1,3 +1,15 @@
-API_VERSION = 'v10'
-CREDENTIALS_PATH = '~/.credentials/googleads.yaml'
-MCC_ID = '***REMOVED***'
+import os
+
+
+def load_env_var(key:str, default: str = None) -> str:
+    env_var = os.getenv(key, default)
+
+    if env_var is None:
+        raise RuntimeError(f'google-ads-api: Required environment variable not set: {key}')
+
+    return env_var
+
+
+API_VERSION = load_env_var('GOOGLE_ADS_API_VERSION',)
+MCC_ID = load_env_var('GOOGLE_ADS_MCC_ID')
+CREDENTIALS_PATH = load_env_var('GOOGLE_ADS_CREDENTIALS_PATH','~/.credentials/googleads.yaml')
